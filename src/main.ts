42e07eea6681ddef36b8a7fb7da493e974a45480
Guilherme <Guilherme.8482@gmail.com>
2019-04-11 23:13:23 -0300

    Added dinamic port support.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { Server } from "./Server"
 import { Client } from "./Client";
+import getPort from 'get-port'
 
-const PORT = 80
+const DEFAULT_PORT = 80
 
 !async function main(){
+    const freePort = await getPort({port: DEFAULT_PORT})
     Client.build()
     const server = new Server
-    server.start(PORT)
+    server.start(freePort)
 }()
