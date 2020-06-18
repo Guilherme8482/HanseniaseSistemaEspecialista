d7c43429ed513f2722faa746c4462aed8b0abff0
Guilherme Rocha <e.guilherme@pucpr.edu.br>
2020-06-18 18:35:29 -0300

    Client build include in static version

---
+++ client/build/precache-manifest.309545d2d7de853c88a3ad3e562041d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9b0cc26a867bcdbae135c441f8a3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fa63b4e9b5251e2aeb7",
+    "url": "/static/js/2.561d0d46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.561d0d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07572318df7a34f7caf6",
+    "url": "/static/js/main.78b08084.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "fef1f20aa76540808c4f5b004bfe49bd",
+    "url": "/static/media/carregando.fef1f20a.gif"
+  }
+]);
