f4d820116245546c1d8e3da7244db393e704ab87
Guilherme Rocha <e.guilherme@pucpr.edu.br>
2020-12-29 21:58:37 -0300

    jar file generated in intellij

--- sistemaHanseniaseJava/Codigo fonte/src/Main.java
+++ sistemaHanseniaseJava/Codigo fonte/src/Main.java
@@ -1,14 +1,15 @@
+
 /**
  *
  * @author Guilherme Rocha
  */
- 
+
 import java.io.IOException;
 
 import norsys.netica.NeticaException;
 
 public class Main {
-	public static void main(String []args) throws IOException, NeticaException{
+	public static void main(String[] args) throws IOException, NeticaException {
 		System.out.print(new GerenciadorDeRede(args).avaliar());
 	}
 }
