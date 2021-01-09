4d0866a81519f4737ca768bbf66f996323bd012c
Guilherme Rocha <e.guilherme@pucpr.edu.br>
2021-01-09 11:48:35 -0300

    Build atualizado

---
+++ client/build/precache-manifest.a994370fbbba8914ca7aa4bce2004bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9316fb9f96866eefcc5b9febb6976e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19976dc757d559cb6a33",
+    "url": "/static/js/2.a3e70ef9.chunk.js"
+  },
+  {
+    "revision": "01fbe32be33ee6656606",
+    "url": "/static/js/main.1e19a30f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fef1f20aa76540808c4f5b004bfe49bd",
+    "url": "/static/media/carregando.fef1f20a.gif"
+  }
+]);
